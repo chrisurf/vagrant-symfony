b0782954fdc627eb7b9b1d3512bf68ff4eb01428
chris <chris@chriss-MacBook-Pro.local>
2021-06-09 18:33:03 +0200

    Quick update

--- app/symfony/migration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Seed Laravel Database
-# IMPORTANT: Check Admin User Role
-# Chris Oguntolu - 15/Apr/2021
-echo Laravel database migration
-cd /var/www/html/symfony
-yarn install && yarn run dev
